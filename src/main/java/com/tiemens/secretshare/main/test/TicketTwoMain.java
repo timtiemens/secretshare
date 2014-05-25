b2f374e65923d173f03809f976e9ac97ead955af
Tim Tiemens <timtiemens@fakeusers.noreply.github.com>
2014-05-25 04:50:32 -0500

    fix copyright

--- src/main/java/com/tiemens/secretshare/main/test/TicketTwoMain.java
+++ src/main/java/com/tiemens/secretshare/main/test/TicketTwoMain.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2014 Tim Tiemens.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ *
+ * Contributors:
+ *     Tim Tiemens - initial API and implementation
+ *******************************************************************************/
 package com.tiemens.secretshare.main.test;
 
 import java.io.UnsupportedEncodingException;
