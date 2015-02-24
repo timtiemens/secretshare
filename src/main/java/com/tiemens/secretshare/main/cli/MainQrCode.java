237b24d3818ec6d7e8a81a5fb8ab0bdaef5a867f
Tim Tiemens <timtiemens@fakeusers.noreply.github.com>
2015-02-24 17:04:14 -0600

    add "main" test framework for zxing

---
+++ src/main/java/com/tiemens/secretshare/main/cli/MainQrCode.java
@@ -0,0 +1,73 @@
+package com.tiemens.secretshare.main.cli;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.Scanner;
+
+import javax.imageio.ImageIO;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+/**
+* @author Crunchify.com
+*/
+
+public class MainQrCode {
+
+    // Tutorial: http://zxing.org/w/docs/javadoc/index.html
+
+    public static void main(String[] args) throws IOException {
+        String myCodeText = "http://Crunchify.com/";
+        String inputFilePath = "build/qr-input.txt";
+        // myCodeText = new String(Files.readAllBytes(Paths.get(inputFilePath)), StandardCharsets.UTF_8); // jdk1.7
+        myCodeText = new Scanner( new File(inputFilePath) ).useDelimiter("\\A").next();
+        String filePath;
+        if (args.length > 0) {
+            filePath = args[0];
+        } else {
+            filePath =  "build/CrunchifyQR.png";
+        }
+        int size = 125;
+        String fileType = "png";
+        File myFile = new File(filePath);
+        try {
+            Hashtable<EncodeHintType, ErrorCorrectionLevel> hintMap = new Hashtable<EncodeHintType, ErrorCorrectionLevel>();
+            hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+            QRCodeWriter qrCodeWriter = new QRCodeWriter();
+            BitMatrix byteMatrix = qrCodeWriter.encode(myCodeText,BarcodeFormat.QR_CODE, size, size, hintMap);
+            int CrunchifyWidth = byteMatrix.getWidth();
+            BufferedImage image = new BufferedImage(CrunchifyWidth, CrunchifyWidth,
+                    BufferedImage.TYPE_INT_RGB);
+            image.createGraphics();
+
+            Graphics2D graphics = (Graphics2D) image.getGraphics();
+            graphics.setColor(Color.WHITE);
+            graphics.fillRect(0, 0, CrunchifyWidth, CrunchifyWidth);
+            graphics.setColor(Color.BLACK);
+
+            for (int i = 0; i < CrunchifyWidth; i++) {
+                for (int j = 0; j < CrunchifyWidth; j++) {
+                    if (byteMatrix.get(i, j)) {
+                        graphics.fillRect(i, j, 1, 1);
+                    }
+                }
+            }
+            ImageIO.write(image, fileType, myFile);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("\n\nYou have successfully created QR Code.");
+    }
+}
+
